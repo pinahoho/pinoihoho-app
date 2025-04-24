1cca471e5a2f7b8f098bedb624559e56371029ad
pinahoho <fadhilapina@gmail.com>
2025-04-24 11:00:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈bersenang senang")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "kita bersenang senang bersama pinoi piupiu yang baik hati dan ramah"
 )
